104d523e04dd5d53d5e996c13322fd7aa19f41dd
ljn261 <90614778+ljn261@users.noreply.github.com>
2025-12-12 12:48:38 +0100

    Update 00_aux_functions.R

--- scripts/00_aux_functions.R
+++ scripts/00_aux_functions.R
@@ -1,6 +1,6 @@
 ## Title: Canary Islands Plant Trait Data Extraction Project - Naturalis Biodiversity Center----
 # This file contains a collection of functions used to extract/clean plant trait data
-# Author: Lucas Jansen (l.s.jansen98@gmail.com)
+# Author: Lucas Jansen 
 # Date: 05/06/2025
 # Written using R version 4.4.1
 
